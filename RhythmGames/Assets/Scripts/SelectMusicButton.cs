70e7b82553e7be3866c069aee440d19ebcc9387b
shinya-tan <ushimiyay20@gmail.com>
2020-08-04 18:25:11 +0900

    add music select view

---
+++ RhythmGames/Assets/Scripts/SelectMusicButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+
+
+public class SelectMusicButton : MonoBehaviour {
+    public Text targetText;
+    private AudioSource audioSource;
+    void Start() {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        this.targetText.text = Resources.Load("Charts/ahurea/ahurea_music").ToString();
+    }
+    
+
+    public void OnClickSelectButton()
+    {
+        SceneManager.LoadScene("pineScene");
+    }
+
+}
+
